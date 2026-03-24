9bd5774c3ca7de1cfe90c8c510d0e27cfcfbd3b1
Hanna Sarna <hannasarna@outlook.com>
2026-03-24 20:28:57 +0100

    coś sie zepsuło

--- SystemLosowaniaOsobyDoOdpowiedziV4/obj/Debug/net9.0-ios/iossimulator-x64/SystemLosowaniaOsobyDoOdpowiedziV4.AssemblyInfo.cs
+++ SystemLosowaniaOsobyDoOdpowiedziV4/obj/Debug/net9.0-ios/iossimulator-x64/SystemLosowaniaOsobyDoOdpowiedziV4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemLosowaniaOsobyDoOdpowiedziV4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+07214329d17d28509f3bbcb10621d3d9cbfb31a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemLosowaniaOsobyDoOdpowiedziV4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemLosowaniaOsobyDoOdpowiedziV4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
